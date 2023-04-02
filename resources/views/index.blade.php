13e520eb198e7b366624445c6d697e980f4581c0
Adam Haley <adamhaley@gmail.com>
2023-04-01 19:00:17 -0700

    added old logos, created logo on home screen

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,16 @@
-HOME
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Adamhaley.com</title>
+</head>
+<body>
+<!-- center div -->
+    <div style="margin: 0 auto; width: 50%; text-align: center;">
+        <img src="/images/AHorb.jpg" alt="Adam Haley" width="200" height="200">
+    </div>
+</body>
+</html>
