4d4bb30655efdf16e83f7947a3d35d5908ee5dde
Adam Haley <adamhaley@gmail.com>
2023-04-12 21:35:56 -0700

    reworked app.css in skins/ice to show AH orb instead of tailwind stuff. using image replacement technique from the early 00's to hide h1 text and show orb as image

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,12 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="skins/ice/css/app.css">
     <title>AdamHaley.com</title>
 </head>
 <body>
 <!-- center div -->
-    <div style="margin: 0 auto; width: 50%; text-align: center;">
-        <img src="/images/AHorb.jpg" alt="Adam Haley" width="200" height="200">
+    <h1 id="ah">
+        <span>AH</span>
     </div>
 </body>
 </html>
