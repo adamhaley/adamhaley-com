823d41a6eee58e4eb7010d34379c6b9c4442bea8
Adam Haley <adamhaley@gmail.com>
2023-04-22 13:46:36 -0700

    Added Breeze/Auth w/ Blade option for view layer

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,43 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\PostsController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
+Route::get('/', function () {
+    return view('index');
+});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 Route::get('/',[PagesController::class, 'index']);
-
 //ah:worx page
 Route::get('/worx',[PagesController::class, 'worx']);
-
 //ah:songs page
 Route::get('/songs',[PagesController::class, 'songs']);
-
 //contact page
 Route::get('/contact',[PagesController::class, 'contact']);
-
 //blog
 Route::resource('blog', PostsController::class);
 
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+require __DIR__.'/auth.php';
