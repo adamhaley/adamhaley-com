5ee0d942ddb1deaa46cf37ee0928c76f0dd9ad7a
Adam Haley <adamhaley@gmail.com>
2023-04-22 18:04:59 -0700

    Created User Seeder, with one admin user , added  public app css and js files to gitignore, made default / route load index view again, added vite to package.json

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\PagesController;
-use App\Http\Controllers\PostsController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,15 +27,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::get('/',[PagesController::class, 'index']);
-//ah:worx page
-Route::get('/worx',[PagesController::class, 'worx']);
-//ah:songs page
-Route::get('/songs',[PagesController::class, 'songs']);
-//contact page
-Route::get('/contact',[PagesController::class, 'contact']);
-//blog
-Route::resource('blog', PostsController::class);
 
 require __DIR__.'/auth.php';
