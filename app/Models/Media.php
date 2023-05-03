27c7926378457b3376026e444fd3b87fe2255b3a
Adam Haley <adamhaley@gmail.com>
2023-05-02 21:35:12 -0700

    Added resource routes, changed "Medium" to "Media"

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -2,10 +2,48 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class media
+ *
+ * @property $id
+ * @property $category_id
+ * @property $name
+ * @property $path
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @property MediaCategory $mediaCategory
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
 class Media extends Model
 {
-    use HasFactory;
+
+    static $rules = [
+		'category_id' => 'required',
+		'name' => 'required',
+		'path' => 'required',
+    ];
+
+    protected $perPage = 20;
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['category_id','name','path'];
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function mediaCategory()
+    {
+        return $this->hasOne('App\Models\MediaCategory', 'id', 'category_id');
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('albums', App\Http\Controllers\AlbumController::class);
+    Route::resource('clients', App\Http\Controllers\ClientController::class);
+    Route::resource('media', App\Http\Controllers\MediaController::class);
+    Route::resource('media-categories', App\Http\Controllers\MediaCategoryController::class);
+    Route::resource('tracks', App\Http\Controllers\TrackController::class);
+    Route::resource('pages', App\Http\Controllers\PagesController::class);
+    Route::resource('posts', App\Http\Controllers\PostController::class);
+    Route::resource('projects', App\Http\Controllers\ProjectController::class);
+    Route::resource('users', App\Http\Controllers\UserController::class);
 });
 
 require __DIR__.'/auth.php';
