e20517a7a3fcb5e21556a6e325a3d5526c93aeef
Adam Haley <adamhaley@gmail.com>
2023-05-29 18:15:03 -0700

    removed jquery-esque event handler code

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,11 +7,3 @@ window.Alpine = Alpine;
 Alpine.start();
 
 console.log('Hello World from app.js!');
-
-$('.navlink').onmouseenter(function(){
-    $('.heading').html('Hello World');
-});
-
-$('.navlink').onmouseleave(function(){
-    $('.heading').html('Goodbye World');
-});
