f4c7037ba621a79495a930afab04189d0914802a
Adam Haley <adamhaley@gmail.com>
2023-05-30 16:18:46 -0700

    adding index.html

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return File::get(public_path() . 'index.html');
 });
 
 Route::get('/dashboard', function () {
