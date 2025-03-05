93ffc32e75f99e7f70966e2e36a389044c627584
adamhaley <adamhaley@gmail.com>
2025-03-05 11:05:24 -0800

    echo whoami

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -4,11 +4,11 @@ set -e
 echo "Deployment started ..."
 
 su adam
+
+echo whoami
 # Enter maintenance mode or return true
 # if already is in maintenance mode
 (php artisan down) || true
-
-
 
 # Pull the latest version of the app
 git pull origin master
