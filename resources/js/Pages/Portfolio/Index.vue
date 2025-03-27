5f994eff5c1ea16549e7ca2edee1f3e1dab43a79
adamhaley <adamhaley@gmail.com>
2025-03-26 22:10:59 -0700

    updated phone num

--- resources/js/Pages/Portfolio/Index.vue
+++ resources/js/Pages/Portfolio/Index.vue
@@ -32,8 +32,8 @@
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500 mr-2" viewBox="0 0 20 20" fill="currentColor">
               <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
             </svg>
-            <a href="tel:+13235551234" class="text-gray-700 hover:text-gray-900">
-              (323) 555-1234
+            <a href="tel:+13233843493" class="text-gray-700 hover:text-gray-900">
+              (323) 384-3493
             </a>
           </div>
         </div>
