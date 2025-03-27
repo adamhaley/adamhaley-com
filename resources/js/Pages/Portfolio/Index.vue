ca47b3a8941472519b5418afd33ecf317466ea48
adamhaley <adamhaley@gmail.com>
2025-03-26 21:28:19 -0700

    content updates

--- resources/js/Pages/Portfolio/Index.vue
+++ resources/js/Pages/Portfolio/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-    <h1 class="text-4xl font-bold text-gray-900 mb-12">ADAM HALEY: Case Studies</h1>
+    <h1 class="text-4xl font-bold text-gray-900 mb-12">ADAM HALEY - Web Developer: Case Studies</h1>
     
     <div class="space-y-24">
       <ProjectSection 
