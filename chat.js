04cf55762847121ecc2557eb9d93780a0e85883a
adamhaley <adamhaley@gmail.com>
2025-06-01 13:04:12 -0700

    reset webhook url to prod url

--- chat.js
+++ chat.js
@@ -7,8 +7,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const reopenButton = document.querySelector('.reopen-chat-button');
     
     // Replace this with your actual webhook URL
-    //const WEBHOOK_URL = 'https://n8n.ahmedia.ai/webhook/3c8f2fca-5a61-4402-887c-a43f6c137e73';
-    const WEBHOOK_URL = 'https://n8n.ahmedia.ai/webhook-test/3c8f2fca-5a61-4402-887c-a43f6c137e73';
+    const WEBHOOK_URL = 'https://n8n.ahmedia.ai/webhook/3c8f2fca-5a61-4402-887c-a43f6c137e73';
+    //const WEBHOOK_URL = 'https://n8n.ahmedia.ai/webhook-test/3c8f2fca-5a61-4402-887c-a43f6c137e73';
 
 
     function createTypingIndicator() {
